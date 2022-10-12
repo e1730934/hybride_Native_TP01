74cf94355eeabbd73fa941a584070eb28677b26f
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-10-12 10:39:22 -0400

    Peaufinage du LoginRepository

--- app/src/main/java/com/inf5d6/tp1/repositories/LoginRepository.kt
+++ app/src/main/java/com/inf5d6/tp1/repositories/LoginRepository.kt
@@ -3,7 +3,6 @@ package com.inf5d6.tp1.repositories
 import android.app.Application
 import android.content.Intent
 import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -15,21 +14,19 @@ class LoginRepository(private val application: Application) {
 
     fun getBearerToken(loginInfo: Login) {
         val queue = Volley.newRequestQueue(application)
-        val url = "https://tvshowdbapi.herokuapp.com/auth/token"
         val jsonBody = JSONObject()
         jsonBody.put("username", loginInfo.username)
         jsonBody.put("password", loginInfo.password)
 
         val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.POST, url, jsonBody,
+            Request.Method.POST, MainActivity.SRVURL+ "/auth/token", jsonBody,
             { response ->
                 if (response != null) {
                     if(response.has("token")){
                         MainActivity.TOKEN.postValue("Bearer " + response.getString("token"))
-                        //Create intent to launch home activity
+
                         val intent = Intent(application, MainActivity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        //Start the activity
                         application.startActivity(intent)
                     }
                 }
@@ -38,10 +35,7 @@ class LoginRepository(private val application: Application) {
                 if (error != null) {
                     Toast.makeText(application, "Error: ${error.message}", Toast.LENGTH_LONG).show()
                 }
-            }
-        )
+            })
         queue.add(jsonObjectRequest)
-
     }
-
 }
